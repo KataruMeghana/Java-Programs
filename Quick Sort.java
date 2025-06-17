d73d76365d850979cc9b4f13dc4c3d1d13f29adf
KataruMeghana <165542702+KataruMeghana@users.noreply.github.com>
2025-06-17 14:10:39 +0530

    Create Quick Sort.java

---
+++ Quick Sort.java
@@ -0,0 +1,41 @@
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {10, 7, 8, 9, 1, 5};
+
+        System.out.println("Original array:");
+        for (int num : numbers) System.out.print(num + " ");
+
+        quickSort(numbers, 0, numbers.length - 1);
+
+        System.out.println("\nSorted array:");
+        for (int num : numbers) System.out.print(num + " ");
+    }
+}
